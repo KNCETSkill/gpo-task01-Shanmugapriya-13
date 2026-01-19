9eafaf04aabb69765b97223ae53439d5fe82c7be
Shanmugapriya-13 <shanmugapriyasrinivasan8@gmail.com>
2026-01-19 16:12:56 +0530

    Update BrewSizeAdvisor.java

--- src/BrewSizeAdvisor.java
+++ src/BrewSizeAdvisor.java
@@ -13,9 +13,9 @@ public class BrewSizeAdvisor {
         // based on the given volume
         if(volume>350)
             System.out.println("Large");
-        else if(volume>250 && volume<=350)
+        else if(volume>=250 && volume<=350)
             System.out.println("Medium");
-        else if(volume>=250)
+        else if(volume>250)
             System.out.println("Small");
         else
             System.out.println("Invalid input,give a positive number")
